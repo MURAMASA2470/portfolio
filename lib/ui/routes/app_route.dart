e6d37b7aac12c155bf6942d2a55c9904270ee423
MURAMASA2470 <keisuke226214@yahoo.co.jp>
2022-02-13 19:07:59 +0900

    routeファイル整形

--- lib/ui/routes/app_route.dart
+++ lib/ui/routes/app_route.dart
@@ -1,27 +1,14 @@
 import 'package:auto_route/auto_route.dart';
+import 'package:portfolio/ui/hobby/hobby_page.dart';
+import 'package:portfolio/ui/home/home_page.dart';
 import 'package:portfolio/ui/layout/master_page.dart';
 import 'package:portfolio/ui/routes/route_path.dart';
-import 'package:portfolio/ui/home/home_page.dart';
-
-import '../hobby/hobby_page.dart';
 
 @AdaptiveAutoRouter(replaceInRouteName: 'Page,Route', routes: [
 
-  AutoRoute(
-    path: "/",
-    page: MasterPage,
-    children: [
-      AutoRoute(
-        path: RoutePath.appRouteHome,
-        page: HomePage,
-        name: "HomeRouter",
-        initial: true,
-      ),
-      AutoRoute(
-          path: RoutePath.appRouteHobby,
-          name: "HobbyRouter",
-          page: HobbyPage
-      ),
+  AutoRoute(path: "/", page: MasterPage, children: [
+      AutoRoute(path: RoutePath.appRouteHome,  page: HomePage,  name: "HomeRouter", initial: true),
+      AutoRoute(path: RoutePath.appRouteHobby, page: HobbyPage, name: "HobbyRouter"),
     ]
   )
 ])
